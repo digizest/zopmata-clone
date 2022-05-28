0b6f646ced286c8b7632c316fd59f2600f98ec26
Ansarimohsin7859 <ansarimohsin7859@gmail.com>
2022-05-28 19:41:19 +0530

    add user schema and user model

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,7 +1,11 @@
+const { userModel } = require("../models/user.model")
+
 const user = [{ name : "asdcw"},{ name : "qweer"},{ name : "abcd"}]
+
 
 //controller 1
 const getListOfUser = (req , res)=>{
+    console.log(userModel)
     return res.status(200).send(user)
 }
 
