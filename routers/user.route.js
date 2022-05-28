ddb03e4ec419e9df30c3cf24ffbc417a4e443e54
Ansarimohsin7859 <ansarimohsin7859@gmail.com>
2022-05-28 19:33:25 +0530

    adding swagger changes , adding api docs files

--- routers/user.route.js
+++ routers/user.route.js
@@ -5,7 +5,7 @@ const { verificationAuth } = require("../middilewares/auth")
 
 //import controllers
 const { userController } = require("../controllers/index")
-//first auth middileware will run then controller will executed
+
 routes.get("/get-user-list" , [ verificationAuth , userController.getListOfUser ])
 
 module.exports = routes
